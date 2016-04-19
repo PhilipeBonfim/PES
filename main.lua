313162c5d292268832ad267af9101839396d0bfd
XHaseo <philipejsb@hotmail.com>
2016-04-19 00:29:53 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,13 +10,16 @@ require "CreateWordTableFromWordList"
 
 function main()
 
+-- Criação das listas das palavras a serem usadas.
 	createWordTableFromWordList(WordList)
 	createWordTableFromWordList(JWordList)
 
+-- Mensagens explicando sobre o programa e como funciona.
 	print("Bem vindo ao tradutor de portugues para japonês.\n")
 	print("Insira uma palavra que deseja em português, para saber como se fala em japonês.\n")
 	print("Para terminar o Programa basta escrever a palavra exit.São válidas palavras com acento e - .\n")
 	
+-- Loop do programa para poder continuar usando, ele é terminado ao inserir a palavra "exit".	
 	while true do 
 		local inputWord = io.read()
 		if(inputWord == "exit") then 
@@ -24,8 +27,9 @@ function main()
 		end
 	
 		local index = CheckWordIndex(inputWord)
-	
-		print("A palavra " .. WordList[index] .. " se pronûncia " .. JWordList[index] .. " em japonês")
+		
+-- Mensagem com a palavra escrita pelo usuário e a sua tradução em japonês.	
+		print("A palavra " .. WordList[index] .. " se pronûncia " .. JWordList[index] .. " em japonês\n")
 	end
 end
 
