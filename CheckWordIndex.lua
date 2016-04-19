0100451fb5a62cec6333db6062906369aabee9d4
XHaseo <philipejsb@hotmail.com>
2016-04-19 08:49:19 -0300

    Update CheckWordIndex.lua

--- CheckWordIndex.lua
+++ CheckWordIndex.lua
@@ -12,7 +12,7 @@ require "Wordtable"
 function GetWordIdexFromWordTable(word)
 	for i = 1, table.getn(WordTable), 1 do
 		if(WordTable[i] == word)
-			return i;
+			return i
 		else
 			print("Palavra não encontrada na tabela de palavras.");
 		end
